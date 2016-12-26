dad95b6b270e77935add1d08ad71faab0089dfdc
hkwkl <sourneco@gmail.com>
2016-12-26 11:17:06 +0900

    [강림]디테일 메뉴 -> 디테일 이미지로 수정. 다른 페이지에서도 클래스값만 넣으면 쓸수있게 수정.

--- gulp_1/public_html/dist/js/detail_img.js
+++ gulp_1/public_html/dist/js/detail_img.js
@@ -1,12 +1,12 @@
 function init(){
-	var arrPrd = document.querySelectorAll(".coffee_wrap li a");
-	var detailBg = document.querySelector(".detail_menu_wrap .detail_menu_bg");
-	var detailWrap = document.querySelector(".detail_menu_wrap");
-	var arrDetail = document.querySelectorAll(".detail_menu_wrap .detail_menu div");
-	for (var i = 0; i < arrPrd.length; i++){
+	var arrImg = document.querySelectorAll(".img_wrap li a");
+	var detailBg = document.querySelector(".detail_img_wrap .detail_img_bg");
+	var detailWrap = document.querySelector(".detail_img_wrap");
+	var arrDetail = document.querySelectorAll(".detail_img_wrap .detail_img div");
+	for (var i = 0; i < arrImg.length; i++){
 		(function (){
 			var k = i;
-			addEvent(arrPrd[i],"click",function(){
+			addEvent(arrImg[i],"click",function(){
 				var menuNum = k;
 				//console.log(menuNum);
 				for (var i = 0; i < arrDetail.length; i++){
