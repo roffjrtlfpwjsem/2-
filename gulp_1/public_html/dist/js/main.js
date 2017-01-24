3c66e48e8cba79ee1879515f1dd862660a36dd69
ye <ye@ye.ye>
2017-01-24 16:16:52 +0900

    [영은]인덱스반응형중(+제이쿼리)

---
+++ gulp_1/public_html/dist/js/main.js
@@ -0,0 +1,156 @@
+var init = function(){
+	var pageBtn = document.querySelectorAll("#idx_section_1 .frame .sliderBannerPage button");
+	var mallBtn = document.querySelectorAll(".mallBtn button");
+	var activePaging = 0;
+	var sUl = document.querySelector("#idx_section_1 .frame ul.visual_wrapPc");
+	var mallUl = document.querySelector(".frame_mall .mall_wrap_in");
+	//var storeFi = document.querySelecor(".open_store_imgWrap figure")
+	var imgWidth = 1200;
+	var showNum = 0;
+	var visualBackground = document.querySelector("#idx_section_1");
+	var maxNum = pageBtn.length-1;
+	var autoSlide = null;
+	var storeBtn = document.querySelectorAll(".storeBtn_idx button");
+	var maxNumSt = storeBtn.length-1;
+	var storeUl = document.querySelector(".open_store ul");
+	var storeFrame = document.querySelector(".open_store .storeFrame");
+	var storeWidth = 239;
+	var activePagingStore = 0;
+	var showNumSt = 0;
+	var newMenuBtn = document.querySelectorAll(".newMenu_wrap .newMenuBtn button");
+	var maxNumNm = newMenuBtn.length-1;
+	var maxNumMa = mallBtn.length-1;
+	var activePagingNm = 0;
+	var activePagingMa = 0;
+	var nmenuUl = document.querySelector(".newMenu_wrap ul");
+	var nMenuWidth = 580;
+	var mallWidth = 552;
+	var showNumNm = 0;
+	var showNumMa = 0;
+
+	/*이미지슬라이드 스크립트*/
+	var playRolling = function(wrap,action_a,action_b,num){
+		addEvent(wrap, "mouseenter",function(){ //마우스벗어났을때 사진움직이기
+			clearInterval(action_a);
+		})
+		addEvent(wrap, "mouseleave",function(){ //마우스 아웃은 한요소 !! 리브를 해야함
+			action_a = setInterval(action_b,num)
+		})
+	}
+	//playRolling(storeUl,autoSlideSt,autoClickStore); 요기서부터 하면된다 불러오기 
+	//autoSlideSt = setInterval(autoClickStore,3000);
+	var autoClick = function(a){
+		showNum++;
+		if(showNum > maxNum){showNum = 0;}
+		//console.log(showNum)
+		playSildeBanner(showNum);
+		chPaging(showNum);
+		chBackground(showNum);
+	}
+	var autoClickStore = function(){
+		showNumSt++;
+		if(showNumSt > maxNumSt){showNumSt = 0;}
+		//console.log(showNumSt)
+		playSildeStore(showNumSt);
+		chStore(showNumSt);
+	}
+	var autoClickNmenu = function(){
+		showNumNm++;
+		if(showNumNm > maxNumNm){showNumNm = 0;}
+		//console.log(showNumNm)
+		playSildeStore(showNumNm);
+		chStore(showNumNm);
+	}
+	var autoClickMall = function(){
+		showNumMa++;
+		if(showNumMa > maxNumMa){showNumMa = 0;}
+		//console.log(showNumMa)
+		playSildeMall(showNumMa);
+		chMall(showNumMa);
+	}
+	autoSlide = setInterval(autoClick,5000);
+	playRolling(visualBackground,autoSlide,autoClick,5000);
+	autoSlidest = setInterval(autoClickStore,2000);
+	playRolling(storeFrame,autoSlidest,autoClickStore,2000);
+	autoSlideMa = setInterval(autoClickMall,3000);
+	playRolling(mallUl,autoSlideMa,autoClickMall,3000);
+	function chBackground(num){
+		var a = num+1;
+		visualBackground.style.backgroundImage = "url(images/main/bg_visual_"+a+".png)";
+	}
+	function playSildeBanner(num){
+		    sUl.style.marginLeft = -num * imgWidth +"px";
+		}
+	function playSildeStore(num){
+	    storeUl.style.marginLeft = -num * storeWidth +"px";
+	}
+	function playSildeNmenu(num){
+	    nmenuUl.style.marginLeft = -num * nMenuWidth +"px";
+	}
+	function playSildeMall(num){
+	    mallUl.style.marginLeft = -num * mallWidth +"px";
+	}
+	var chPaging = function(num){
+			pageBtn[activePaging].setAttribute("class","");
+			pageBtn[num].setAttribute("class","on");
+			activePaging = num;
+		}
+	var chStore = function(num){
+			storeBtn[activePagingStore].setAttribute("class","");
+			storeBtn[num].setAttribute("class","on");
+			activePagingStore = num;
+		}
+	var chNmenu = function(num){
+			newMenuBtn[activePagingNm].setAttribute("class","");
+			newMenuBtn[num].setAttribute("class","on");
+			activePagingNm = num;
+		}
+	var chMall = function(num){
+			mallBtn[activePagingMa].setAttribute("class","");
+			mallBtn[num].setAttribute("class","on");
+			activePagingMa = num;
+		}	
+	for(var i=0;i<pageBtn.length;i++){ //비쥬얼버튼
+			(function(){
+				var k = i;
+				addEvent(pageBtn[i],"click",function(){
+					chPaging(k);
+					playSildeBanner(k);
+					chBackground(k);
+					showNum = k;
+				});
+			}());
+		}
+	for(var i=0;i<storeBtn.length;i++){
+			(function(){
+				var k = i;
+				addEvent(storeBtn[i],"click",function(){
+					chStore(k);
+					playSildeStore(k);
+					showNumSt = k;
+				});
+			}());
+		}
+	for(var i=0;i<newMenuBtn.length;i++){
+		(function(){
+			var k = i;
+			addEvent(newMenuBtn[i],"click",function(){
+				chNmenu(k);
+				playSildeNmenu(k);
+				showNumNm = k;
+			});
+		}());
+	}
+	for(var i=0;i<mallBtn.length;i++){
+		(function(){
+			var k = i;
+			addEvent(mallBtn[i],"click",function(){
+				chMall(k);
+				playSildeMall(k);
+				showNumMa = k;
+			});
+		}());
+	}
+	/*//이미지슬라이드 스크립트*/
+	}
+	addEvent(window,"load",init)
